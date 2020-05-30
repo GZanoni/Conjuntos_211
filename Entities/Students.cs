15b1aa3dfbb3e20e294ba18e642ed615bf64b364
GZanoni <gean.zanoni@yahoo.com.br>
2020-05-29 21:14:58 -0300

    Exericio proposto da aula 211 finalizado

---
+++ Entities/Students.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sets_Ex_211.Entities
+{
+    class Students
+    {
+        public int Code { get; set; }
+        public int N { get; set; }
+        HashSet<int> set = new HashSet<int>();
+
+        public Students()
+        {
+        }
+
+        public Students(int code, int n)
+        {
+            Code = code;
+            N = n;
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+
+
+        public void Quantity(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Code = int.Parse(Console.ReadLine());
+                set.Add(Code);
+
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "Total students: " + set.Count;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Students))
+            {
+                throw new Exception("The student doesn't belong to any course!");
+            }
+
+            Students other = obj as Students;
+            return Code.Equals(other.Code);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Sets_Ex_211.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Sets_Ex_211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Students stu = new Students();
+
+                Console.Write("How many students for the course A: ");
+                int n = int.Parse(Console.ReadLine());
+                stu.Quantity(n);
+
+                Console.Write("How many students for the course B: ");
+                n = int.Parse(Console.ReadLine());
+                stu.Quantity(n);
+                
+                Console.Write("How many students for the course C: ");
+                n = int.Parse(Console.ReadLine());
+                stu.Quantity(n);
+
+
+                Console.WriteLine(stu);
+
+            }
+            catch (IOException)
+            {
+                throw new IOException("The type value is not correct");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
